83ffcc6e53fc3922df217089e5e0052454ac8757
Minstones <mumin96303@gmail.com>
2024-02-05 22:34:49 +0700

    update

--- mysite/face_recog/recog.py
+++ mysite/face_recog/recog.py
@@ -1,20 +1,22 @@
-from django.utils import timezone
-from datetime import datetime
 import cv2
 from mtcnn.mtcnn import MTCNN
 from deepface import DeepFace
 from .models import Transaction
 from django.core.files import File
 import os
-import threading
 import tempfile
 from pathlib import Path
 from django.conf import settings
-
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from django.utils import timezone
+import threading
 
 class FaceRecognition:
     def __init__(self):
         self.stay_still_arr = []
+        self.stay_still_lock = threading.Lock()  # Lock for synchronization
+        self.thread_pool = ThreadPoolExecutor(max_workers=4)
 
     def face_compare(self, result, temp_face_path):
         if len(result[0]) == 0:
@@ -31,64 +33,66 @@ class FaceRecognition:
             EmployeeID = EmployeeID.replace(':', '')
             Name = Name.replace(':', '')
 
-            employee_in_arr = False
-            for item in self.stay_still_arr:
-                if item['EmployeeID'] == EmployeeID:
-                    employee_in_arr = True
-                    time_difference = timezone.now() - datetime.strptime(item['DateTime'], '%Y-%m-%d %H:%M:%S')
-                    if time_difference.total_seconds() > 300:
-                        # post data
-                        transaction = Transaction(EmployeeID=EmployeeID, Name=Name, DateTime=timezone.now().strftime('%Y-%m-%d %H:%M:%S'), CameraNo=1, Image=File(open(Path(temp_face_path).relative_to(settings.BASE_DIR), "rb")))
-                        transaction.save()
-                        self.stay_still_arr.append({'EmployeeID': EmployeeID, 'Name': Name, 'DateTime': timezone.now().strftime('%Y-%m-%d %H:%M:%S')})
-            if not employee_in_arr:
-                # post data
-                transaction = Transaction(EmployeeID=EmployeeID, Name=Name, DateTime=timezone.now().strftime('%Y-%m-%d %H:%M:%S'), CameraNo=1, Image=File(open(Path(temp_face_path).relative_to(settings.BASE_DIR), "rb")))
-                transaction.save()
-                self.stay_still_arr.append({'EmployeeID': EmployeeID, 'Name': Name, 'DateTime': timezone.now().strftime('%Y-%m-%d %H:%M:%S')})
+            # Acquire lock before accessing stay_still_arr
+            with self.stay_still_lock:
+                employee_in_arr = False
+                for item in self.stay_still_arr:
+                    if item['EmployeeID'] == EmployeeID:
+                        employee_in_arr = True
+                        time_difference = timezone.now() - datetime.strptime(item['DateTime'], '%Y-%m-%d %H:%M:%S')
+                        if time_difference.total_seconds() > 300:
+                            # post data
+                            transaction = Transaction(EmployeeID=EmployeeID, Name=Name, DateTime=timezone.now().strftime('%Y-%m-%d %H:%M:%S'), CameraNo=1, Image=File(open(Path(temp_face_path).relative_to(settings.BASE_DIR), "rb")))
+                            transaction.save()
+                            self.stay_still_arr.append({'EmployeeID': EmployeeID, 'Name': Name, 'DateTime': timezone.now().strftime('%Y-%m-%d %H:%M:%S')})
+                if not employee_in_arr:
+                    # post data
+                    transaction = Transaction(EmployeeID=EmployeeID, Name=Name, DateTime=timezone.now().strftime('%Y-%m-%d %H:%M:%S'), CameraNo=1, Image=File(open(Path(temp_face_path).relative_to(settings.BASE_DIR), "rb")))
+                    transaction.save()
+                    self.stay_still_arr.append({'EmployeeID': EmployeeID, 'Name': Name, 'DateTime': timezone.now().strftime('%Y-%m-%d %H:%M:%S')})
+
+        # Release lock after accessing stay_still_arr
         # Remove after face_compare
         os.remove(temp_face_path)
 
+    def face_process(self, temp_face_path):
+        result = DeepFace.find(
+            img_path=temp_face_path,
+            db_path="C:/DriveD/Face recog website/mysite/face_recog/faces",
+            model_name='VGG-Face',
+            enforce_detection=False
+        )
+        self.face_compare(result, temp_face_path)
 
     def run_recognition(self):
-        # Load the pre-trained MTCNN model
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        # Capture video from the default camera (use 0)
         cap = cv2.VideoCapture(0)
 
         while True:
             ret, frame = cap.read()
             small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
             rgb_small_frame = small_frame[:, :, ::-1]
-            # Detect faces using MTCNN
             faces = face_cascade.detectMultiScale(rgb_small_frame, scaleFactor=1.1, minNeighbors=5, minSize=(80, 80))
             temp_dir = 'transaction_img'
+
             for (x, y, w, h) in faces:
-                # Capture and save the face region as an image
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
                 face_image = frame[y:y+h, x:x+w]
 
-                with tempfile.NamedTemporaryFile(suffix='.jpg',dir=temp_dir, delete=False) as temp_face_file:
+                with tempfile.NamedTemporaryFile(suffix='.jpg', dir=temp_dir, delete=False) as temp_face_file:
                     temp_face_path = temp_face_file.name
                     cv2.imwrite(temp_face_path, face_image)
-                    # Perform facial recognition using DeepFace on the captured face region
-                    result = DeepFace.find(
-                        img_path=temp_face_path,  # Use the captured face region directly
-                        db_path="C:/DriveD/Face recog website/mysite/face_recog/faces",
-                        model_name='VGG-Face',
-                        enforce_detection=False
-                    )
-                    # Use threading to perform face comparison
-                    thread = threading.Thread(target=self.face_compare, args=(result,temp_face_path))
-                    thread.start()
+                    self.thread_pool.submit(self.face_process, temp_face_path)
 
-            # Display the frame
             cv2.imshow('Face Detection', frame)
 
-            # Break the loop if 'q' key is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         cap.release()
         cv2.destroyAllWindows()
 
+
+if __name__ == "__main__":
+    fr = FaceRecognition()
+    fr.run_recognition()
