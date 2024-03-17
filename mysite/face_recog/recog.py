4d2a0753ef050c27806fd6d4226e80c01d6201d6
Minstones <mumin96303@gmail.com>
2024-03-17 16:13:07 +0700

    17/3/24

--- mysite/face_recog/recog.py
+++ mysite/face_recog/recog.py
@@ -1,3 +1,4 @@
+# For x = s.split('/')[<number>] if in production use 3 else you need to justify
 import cv2
 from deepface import DeepFace
 from .models import Transaction
@@ -11,7 +12,10 @@ from datetime import datetime
 from django.utils import timezone
 import threading
 from django.http import StreamingHttpResponse
+import requests
+from dotenv import load_dotenv
 
+load_dotenv('.env')
 
 class FaceRecognition:
     def __init__(self):
@@ -20,28 +24,32 @@ class FaceRecognition:
         self.stay_still_lock = threading.Lock()  # Lock for synchronization
         self.thread_pool = ThreadPoolExecutor(max_workers=1)
 
-    # def check_stranger(self, encode, threshold=0.6):
-    #     for item in self.stay_still_arr_stranger:
-    #         distance = face_recognition.face_distance([encode], item)[0]
-    #         similarity = distance < threshold
-    #         if similarity == True:
-    #             pass
-    #         else:
-    #             return similarity
+    def send_data_to_api(self, data, temp_face_path):
+        print("send_data_to_api")
+        api_url = f'{os.getenv("SERVER")}/api/addTransaction'  # Replace with your API endpoint URL
+        files = {'Image': open(temp_face_path, 'rb')}
+        response = requests.post(api_url, data=data, files=files)
+        if response.status_code == 201:  # Assuming your API returns 201 for success
+            print("Data sent successfully")
+        else:
+            print("Failed to send data:", response.status_code)
 
-    def face_compare(self, result, temp_face_path,time):
+    def face_compare(self, result, temp_face_path,time,st):
         if len(result[0]) == 0:
             print('not found!!!')
             pass
         else:
             s = result[0]['identity'][0]
             print(s)
-            x = s.split('/')[6]
+            x = s.split('/')[3]
             EmployeeID = x.split('_')[0] #EmpID
             Name = x.split('_')[1]
-            print(f'Name = {Name}')
+
             EmployeeID = EmployeeID.replace(':', '')
             Name = Name.replace(':', '')
+
+            print(f'EmployeeID------>{EmployeeID}')
+            print(f'Name------>{Name}')
 
             # Acquire lock before accessing stay_still_arr
             with self.stay_still_lock:
@@ -52,20 +60,37 @@ class FaceRecognition:
                         time_difference = time - datetime.strptime(item['DateTime'], '%Y-%m-%d %H:%M:%S')
                         if time_difference.total_seconds() > 300:
                             # post data
-                            transaction = Transaction(EmployeeID=EmployeeID, Name=Name, DateTime=time.strftime('%Y-%m-%d %H:%M:%S'), CameraNo=int(os.environ.get('CAMERA_VALUE', 1)), Image=File(open(Path(temp_face_path).relative_to(settings.BASE_DIR), "rb")))
-                            transaction.save()
+                            # transaction = Transaction(EmployeeID=EmployeeID, Name=Name, DateTime=time.strftime('%Y-%m-%d %H:%M:%S'), CameraNo=int(os.environ.get('CAMERA_VALUE', 1)), Image=File(open(Path(temp_face_path).relative_to(settings.BASE_DIR), "rb")))
+                            # transaction.save()
+                            data = {
+                                'EmployeeID': EmployeeID,
+                                'Name': Name,
+                                'DateTime': time.strftime('%Y-%m-%d %H:%M:%S'),
+                                'CameraNo': int(os.getenv('CAMERA_NUMBER')),
+                            }
+                            self.send_data_to_api(data, temp_face_path)
                             self.stay_still_arr.append({'EmployeeID': EmployeeID, 'Name': Name, 'DateTime': time.strftime('%Y-%m-%d %H:%M:%S')})
                 if not employee_in_arr:
                     # post data
-                    transaction = Transaction(EmployeeID=EmployeeID, Name=Name, DateTime=time.strftime('%Y-%m-%d %H:%M:%S'), CameraNo=int(os.environ.get('CAMERA_VALUE', 1)), Image=File(open(Path(temp_face_path).relative_to(settings.BASE_DIR), "rb")))
-                    transaction.save()
+                    # transaction = Transaction(EmployeeID=EmployeeID, Name=Name, DateTime=time.strftime('%Y-%m-%d %H:%M:%S'), CameraNo=int(os.environ.get('CAMERA_VALUE', 1)), Image=File(open(Path(temp_face_path).relative_to(settings.BASE_DIR), "rb")))
+                    # transaction.save()
+                    data = {
+                        'EmployeeID': EmployeeID,
+                        'Name': Name,
+                        'DateTime': time.strftime('%Y-%m-%d %H:%M:%S'),
+                        'CameraNo': int(os.getenv('CAMERA_NUMBER')),
+                    }
+                    self.send_data_to_api(data, temp_face_path)
                     self.stay_still_arr.append({'EmployeeID': EmployeeID, 'Name': Name, 'DateTime': time.strftime('%Y-%m-%d %H:%M:%S')})
 
         # Release lock after accessing stay_still_arr
         # Remove after face_compare
         os.remove(temp_face_path)
+        et=datetime.now()
+        print("total time consumeed ---->", (et-st).total_seconds())
 
     def face_process(self, temp_face_path):
+        st = datetime.now()
         result = DeepFace.find(
             img_path=temp_face_path,
             db_path="face_recog/faces",
@@ -73,7 +98,7 @@ class FaceRecognition:
             enforce_detection=False
         )
         time = timezone.now()
-        self.face_compare(result, temp_face_path,time)
+        self.face_compare(result, temp_face_path,time,st)
 
     def run_recognition(self):
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
